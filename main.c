a4d51c59881c4f02bbcb05ea6556870501c4cae6
Romano Schoonheim <stal@romanoschoonheim.nl>
2024-06-18 11:20:25 +0200

    Clean-up.

--- main.c
+++ main.c
@@ -1,5 +1,29 @@
 #include "modules/open_gl/open_gl.h"
 #include "modules/mesh_grid/mesh_grid.h"
+#include <GL/glu.h>
+
+/**
+ * @brief Creates a fullscreen openGL window.
+ * @return {GLFWwindow *} The window.
+ */
+GLFWwindow *create_fullscreen_window() {
+    const GLFWvidmode *mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+    if (mode == NULL) {
+        return NULL;
+    }
+    GLFWwindow *window = glfwCreateWindow(mode->width, mode->height, "C Physics", glfwGetPrimaryMonitor(), NULL);
+    glfwMakeContextCurrent(window);
+
+    return window;
+}
+
+/**
+ * @brief Creates a mesh_grid_t object.
+ * @return {mesh_grid_t *} The mesh grid.
+ */
+mesh_grid_t *create_mesh_grid() {
+    return mesh_grid_create(1200, 1200, 1200);
+}
 
 int main(void) {
 
@@ -7,17 +31,26 @@ int main(void) {
         return 1;
     }
 
-    int width = 800;
-    int height = 600;
-    int depth = 800;
+    GLFWwindow *window = create_fullscreen_window();
+    if (window == NULL) {
+        return 1;
+    }
 
-    GLFWwindow *window = open_gl_create_window(width, height, "C-Physics");
-
-    mesh_grid_t *mesh_grid = mesh_grid_create(width, width, depth);
-
+    mesh_grid_t *mesh_grid = create_mesh_grid();
 
     while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // When escape is pressed, close the window
+        //
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+        }
+
+
+        glfwSwapBuffers(window);
         glfwPollEvents();
+
     }
 
 
